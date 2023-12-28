7ad601f584a5a04e2f2ea21f8723d3fba4b6eff4
Rodrigo Braz <braz.rodrigo94@gmail.com>
2023-12-28 16:53:54 -0300

    Project Created

---
+++ src/test/kotlin/br/com/rodrigobraz/restspringbootkotlin/MainTests.kt
@@ -0,0 +1,13 @@
+package br.com.rodrigobraz.restspringbootkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
